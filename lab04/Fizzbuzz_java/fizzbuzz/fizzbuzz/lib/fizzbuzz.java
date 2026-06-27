7b59e2af9b84f44c9906e45b5f479c49f944b227
Omar Almoghly <oalmo045@uottawa.ca>
2026-06-27 02:01:21 -0400

    GREEN+REFACTOR: liste fizzbuzz(start,end) implementee, logique simplifiee en if/else if - 10/10 tests passent

--- lab04/Fizzbuzz_java/fizzbuzz/fizzbuzz/lib/fizzbuzz.java
+++ lab04/Fizzbuzz_java/fizzbuzz/fizzbuzz/lib/fizzbuzz.java
@@ -1,23 +1,27 @@
 package lib;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Fizzbuzz {
 
     public String fizzbuzz(int n) {
-        if (n % 15 == 0 && n != 0) {
+        if (n != 0 && n % 15 == 0) {
             return "FizzBuzz";
-        }
-        if (n % 3 == 0 && n != 0) {
+        } else if (n != 0 && n % 3 == 0) {
             return "Fizz";
-        }
-        if (n % 5 == 0 && n != 0) {
+        } else if (n != 0 && n % 5 == 0) {
             return "Buzz";
+        } else {
+            return String.valueOf(n);
         }
-        return String.valueOf(n);
     }
 
     public List<String> fizzbuzz(int start, int end) {
-        return null;
+        List<String> result = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            result.add(fizzbuzz(i));
+        }
+        return result;
     }
 }
