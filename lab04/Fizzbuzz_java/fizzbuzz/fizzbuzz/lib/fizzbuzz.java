e4fe8f6121e88de61b356a54f29734052d32deb1
Omar Almoghly <oalmo045@uottawa.ca>
2026-06-27 01:59:01 -0400

    GREEN: logique Buzz et FizzBuzz ajoutee - 9/10 tests passent. Liste pas encore geree

--- lab04/Fizzbuzz_java/fizzbuzz/fizzbuzz/lib/fizzbuzz.java
+++ lab04/Fizzbuzz_java/fizzbuzz/fizzbuzz/lib/fizzbuzz.java
@@ -5,8 +5,14 @@ import java.util.List;
 public class Fizzbuzz {
 
     public String fizzbuzz(int n) {
+        if (n % 15 == 0 && n != 0) {
+            return "FizzBuzz";
+        }
         if (n % 3 == 0 && n != 0) {
             return "Fizz";
+        }
+        if (n % 5 == 0 && n != 0) {
+            return "Buzz";
         }
         return String.valueOf(n);
     }
